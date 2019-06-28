745cba19cca8c43dcbf9510cc57fef17c570fcd5
dylanreinsma <dylanreinsma@gmail.com>
2019-06-28 11:17:21 -0700

    initial upload

---
+++ app/src/main/java/com/dylanreinsma/whatstheweather/MainActivity.java
@@ -0,0 +1,27 @@
+package com.dylanreinsma.whatstheweather;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button button;
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView2 = findViewById(R.id.textView2);
+    }
+}
